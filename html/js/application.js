222d9fa49b1467d31ce9b89ce634c88266281266
Denis Narush <child.denis@gmail.com>
2016-09-15 13:49:53 +0300

    added fields and drag image event

--- html/js/application.js
+++ html/js/application.js
@@ -1,4 +1,4 @@
-/*global FileReader*/
+/*global FileReader, console*/
 (function (window) {
     'use strict';
 
@@ -18,8 +18,33 @@
         try {
             reader.readAsDataURL(this.files[0]);
         } catch (e) {
-
         }
-
     });
+
+    var holder = document.querySelector('.postcard__side_front');
+
+    holder.ondragover = function () {
+        holder.classList.add('hover');
+        return false;
+    };
+    holder.ondragleave = function () {
+        holder.classList.remove('hover');
+        return false;
+    };
+    holder.ondragend = function () {
+        holder.classList.remove('hover');
+        return false;
+    };
+    holder.ondrop = function (e) {
+        holder.classList.remove('hover');
+        holder.classList.add('loaded');
+        e.preventDefault();
+
+        var file = e.dataTransfer.files[0];
+
+        reader.readAsDataURL(file);
+
+        return false;
+    };
+  
 }(this));
