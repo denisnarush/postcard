ef25ed10f4b9d7657bf19a4e03b548b210985138
Andrei Zaman <azaman@exadel.com>
2016-09-16 18:00:58 +0300

    UI changes

--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,24 @@
-/*global FileReader*/
+/*global FileReader, console*/
 (function (window) {
     'use strict';
 
-    var reader = new FileReader();
+    var reader = new FileReader(event);
+    var i;
+    
+    function backHandler() {
+        document.querySelector('.postcard').classList.remove('postcard_sideone');
+        document.querySelector('.postcard').classList.add('postcard_sidetwo');
+        event.target.setAttribute('hidden', '');
+        event.target.removeEventListener('click', backHandler);
+        document.querySelectorAll('.button')[1].setAttribute('hidden', '');
+    }
+    function validate() {
+        if (document.querySelector('form').checkValidity()) {
+            document.querySelectorAll('.button')[1].removeAttribute('hidden');
+        } else {
+            document.querySelectorAll('.button')[1].setAttribute('hidden', '');
+        }
+    }
 
     reader.addEventListener('load', function (e) {
 
@@ -10,7 +26,13 @@
         document.querySelector('.postcard__side_front').classList.add('loaded');
 
         setTimeout(function () {
+            document.querySelector('.postcard').classList.remove('postcard_sidetwo');
             document.querySelector('.postcard').classList.add('postcard_sideone');
+            setTimeout(function () {
+                document.querySelectorAll('.button')[0].removeAttribute('hidden');
+                document.querySelectorAll('.button')[0].addEventListener('click', backHandler);
+                validate();
+            }, 1100);
         }, 500);
     });
 
@@ -18,8 +40,38 @@
         try {
             reader.readAsDataURL(this.files[0]);
         } catch (e) {
-
         }
-
     });
+
+    var fields = document.querySelectorAll('textarea');
+    for (i = 0; i < fields.length; i = i + 1) {
+        fields[i].addEventListener('input', validate);
+    }
+
+    var holder = document.querySelector('.postcard__side_front');
+
+    holder.ondragover = function () {
+        holder.classList.add('hover');
+        return false;
+    };
+    holder.ondragleave = function () {
+        holder.classList.remove('hover');
+        return false;
+    };
+    holder.ondragend = function () {
+        holder.classList.remove('hover');
+        return false;
+    };
+    holder.ondrop = function (e) {
+        holder.classList.remove('hover');
+        holder.classList.add('loaded');
+        e.preventDefault();
+
+        var file = e.dataTransfer.files[0];
+
+        reader.readAsDataURL(file);
+
+        return false;
+    };
+  
 }(this));
