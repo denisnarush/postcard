e341695cac48e9678ec97314a2e29cab428de149
Denis Narush <child.denis@gmail.com>
2016-09-20 18:25:44 +0300

    attached html partial mechanism

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 /* jshint node: true */
 var gulp = require('gulp');
 var less = require('gulp-less');
+var pug = require('gulp-pug');
 /**
  * Builds style file
  */
@@ -10,9 +11,17 @@ gulp.task('less', function () {
   .pipe(gulp.dest('./public/'));
 });
 /**
+ * Builds view file
+ */
+gulp.task('views', function () {
+  return gulp.src('./src/index.pug')
+  .pipe(pug())
+  .pipe(gulp.dest('./public/'));
+});
+/**
  * Builds public project
  */
-gulp.task('build', ['less'], function () {
+gulp.task('build', ['less', 'views'], function () {
 });
 /**
  * Default task
